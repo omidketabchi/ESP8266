a5f5ca9e76ab1e5a8dbf8565740b237de084308c
Omid Ketabchi <ketabchi.omid@hotmail.com>
2019-04-28 10:07:51 -0700

    add arduino project

---
+++ Sketch1/Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	4/28/2019 10:07:02 AM
+ Author:	O.Ketabchi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
